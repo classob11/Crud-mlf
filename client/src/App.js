1189080123010f248c25f20866aec54e30281ee9
Christian Lasso <lassoc822@gmail.com>
2023-12-06 16:38:35 -0600

    enable

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,123 @@
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
+import Axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 
-  const [puesto,setPuesto] = useState("");
-  const [nombre,setNombre] = useState("");
-  const [correo,setCorreo] = useState("");
-  const [pass,setPass] = useState("");
-  const [ocho,setOcho] = useState("");
-  const [mycase,setMycase] = useState("");
+  const [puesto, setPuesto] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [pass, setPass] = useState("");
+  const [ocho, setOcho] = useState("");
+  const [mycase, setMycase] = useState("");
 
-  const mostrarDatos = ()=>{
-    alert(puesto);
+  const [empleadosList, setEmpleados] = useState([]);
+
+  const add = () => {
+    Axios.post("http://localhost:3001/create", {
+      puesto: puesto,
+      nombre: nombre,
+      correo: correo,
+      pass: pass,
+      ocho: ocho,
+      mycase: mycase
+    }).then(() => {
+      getEmpleados();
+      alert("Empleado Registrado");
+    });
   }
 
+  const getEmpleados = () => {
+    Axios.get("http://localhost:3001/empleados").then((response) => {
+      setEmpleados(response.data);
+    });
+  }
+
+  //getEmpleados();
+
+
   return (
-    <div className="App">
-      <div className='datos'>
-        <label>Puesto: <input 
-        onChange={(event)=>{
-          setPuesto(event.target.value);
-        }}
-        type='text'/> </label>
+    <div class="container">
+      <div className="App">
 
-        <label>Nombre: <input 
-        onChange={(event)=>{
-          setNombre(event.target.value);
-        }}
-        type='text'/> </label> 
+        <div className='lista'>
+          <button onClick={getEmpleados}>Listar</button>
+          {
+            empleadosList.map((val, key) => {
+              return <div className=''> {val.puesto} </div>
+            })
+          }
+        </div>
 
-        <label>Correo: <input 
-        onChange={(event)=>{
-          setCorreo(event.target.value);
-        }}
-        type='text'/> </label>
-
-        <label>Contraseña: <input 
-        onChange={(event)=>{
-          setPass(event.target.value);
-        }}
-        type='text'/> </label>
-
-        <label>8x8: <input 
-        onChange={(event)=>{
-          setOcho(event.target.value);
-        }}
-        type='text'/> </label> 
-
-        <label>MyCase: <input 
-        onChange={(event)=>{
-          setMycase(event.target.value);
-        }}
-        type='text'/> </label>
-
-        <button onClick={mostrarDatos}>Registrar</button>
       </div>
+      <div class="card text-center">
+        <div class="card-header">
+          Gestion de empleados
+        </div>
+        <div class="card-body">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Puesto:</span>
+            <input 
+            onChange={(event) => {
+              setPuesto(event.target.value);
+            }}
+            type="text" class="form-control" placeholder="Puesto" aria-label="Puesto" aria-describedby="basic-addon1"/>
+          </div>
+
+
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Nombre:</span>
+            <input 
+            onChange={(event) => {
+              setNombre(event.target.value);
+            }}
+            type="text" class="form-control" placeholder="Nombre Completo" aria-label="Nombre Completo" aria-describedby="basic-addon1"/>
+          </div>
+
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">@</span>
+            <input 
+            onChange={(event) => {
+              setCorreo(event.target.value);
+            }}
+            type="email" class="form-control" placeholder="Correo" aria-label="Correo" aria-describedby="basic-addon1"/>
+          </div>
+
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Contraseña</span>
+            <input 
+            onChange={(event) => {
+              setPass(event.target.value);
+            }}
+            type="password" class="form-control" placeholder="Contraseña" aria-label="Contraseña" aria-describedby="basic-addon1"/>
+          </div>
+
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Celular</span>
+            <input 
+            onChange={(event) => {
+              setOcho(event.target.value);
+            }}
+            type="text" class="form-control" placeholder="+52" aria-label="Numero" aria-describedby="basic-addon1"/>
+          </div>
+
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Celular</span>
+            <input 
+            onChange={(event) => {
+              setMycase(event.target.value);
+            }}
+            type="text" class="form-control" placeholder="MyCase" aria-label="MyCase" aria-describedby="basic-addon1"/>
+          </div>
+
+        </div>
+        <div class="card-footer text-muted">
+          <button className='btn btn-success' onClick={add}>Registrar</button>
+        </div>
+      </div>
+
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,50 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"empleados_crud"
+})
+
+app.post("/create",(req,res)=>{
+    const puesto = req.body.puesto;
+    const nombre = req.body.nombre;
+    const correo = req.body.correo;
+    const pass = req.body.pass;
+    const ocho = req.body.ocho;
+    const mycase = req.body.mycase;
+    
+
+    db.query('INSERT INTO empleados(puesto,nombre,correo,pass,ocho,mycase) VALUES(?,?,?,?,?,?)',[puesto,nombre,correo,pass,ocho,mycase],
+        (err,result)=>{
+            if (err) {
+                console.log(err);
+            }else{
+                res.send("Empleado registrado con exito!!")
+            }
+    }
+    );
+})
+
+app.get("/empleados",(req,res)=>{
+    db.query('SELECT * FROM empleados',
+        (err,result)=>{
+            if (err) {
+                console.log(err);
+            }else{
+                res.send(result);
+            }
+    }
+    );
+})
 
 app.listen(3001,()=>{
-    console.log("Corriendo en el puerto 30001")
+    console.log("Corriendo en el puerto 3001")
 })
